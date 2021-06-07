07ccc7823872db693a14c96f5604274ead41a586
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-06-07 19:51:50 +0100

    javascript better

--- main.js
+++ main.js
@@ -6,62 +6,66 @@ const time = document.getElementById("time");
 const text = document.getElementById("text");
 
 //to default ()
-const breathAnimation = () => {
-  my1 = setTimeout(function () {
-    isPlaying
-      ? (text.innerText = "Breathe In!")
-      : (text.innerText = "Restart!");
-    circle.className = "breathe__circle slowGrow";
-    my2 = setTimeout(function () {
-      isPlaying
-        ? (text.innerText = "Breathe In!")
-        : (text.innerText = "Restart!");
-      circle.className = "breathe__circle grow";
-      my3 = setTimeout(function () {
-        isPlaying ? (text.innerText = "Hold!") : (text.innerText = "Restart!");
-        circle.className = "breathe__circle hold";
-        my4 = setTimeout(function () {
-          isPlaying
-            ? (text.innerText = "Breathe out!")
-            : (text.innerText = "Restart!");
-          circle.className = "breathe__circle shrink";
-          my5 = setTimeout(function () {
-            isPlaying
-              ? (text.innerText = "Breathe out!")
-              : (text.innerText = "Restart!");
-            circle.className = "breathe__circle slowShrink";
-          }, 2950);
-        }, 2000);
-      }, 2950);
-    }, 0050);
-  }, 0050);
-};
+// const breathAnimation = () => {
+//   my1 = setTimeout(function () {
+//     isPlaying
+//       ? (text.innerText = "Breathe In!")
+//       : (text.innerText = "Wait for restart!");
+//     circle.className = "breathe__circle slowGrow";
+//     my2 = setTimeout(function () {
+//       isPlaying
+//         ? (text.innerText = "Breathe In!")
+//         : (text.innerText = "Wait for restart!");
+//       circle.className = "breathe__circle grow";
+//       my3 = setTimeout(function () {
+//         isPlaying
+//           ? (text.innerText = "Hold!")
+//           : (text.innerText = "Wait for restart!");
+//         circle.className = "breathe__circle hold";
+//         my4 = setTimeout(function () {
+//           isPlaying
+//             ? (text.innerText = "Breathe out!")
+//             : (text.innerText = "Wait for restart!");
+//           circle.className = "breathe__circle shrink";
+//           my5 = setTimeout(function () {
+//             isPlaying
+//               ? (text.innerText = "Breathe out!")
+//               : (text.innerText = "Restart!");
+//             circle.className = "breathe__circle slowShrink";
+//           }, 2950);
+//         }, 2000);
+//       }, 2950);
+//     }, 0050);
+//   }, 0050);
+// };
 
 // Animation()
 const pauseButton = document.getElementById("pause-button");
 const imagePause = document.getElementById("image-pause");
 let isPlaying = false;
-pauseButton.disabled = false;
-const pauseAnimation = () => {
-  if (!isPlaying) {
-    imagePause.src = "./img/repeat.svg";
-    isPlaying = true;
-    pauseButton.disabled = false;
-    breathAnimation();
-    startBreathing = setInterval(breathAnimation, 8000);
-    startTimer();
-    timingInterval = setInterval(startTimer, 1000);
-  } else {
-    console.log(circle.classList);
-    pauseButton.disabled = true;
-    imagePause.src = "./img/play.svg";
-    isPlaying = false;
-    clearInterval(timingInterval);
-    clearInterval(startBreathing);
-  }
-};
+// pauseButton.disabled = false;
+// const pauseAnimation = () => {
+//   console.log("working");
+//   if (!isPlaying) {
+//     imagePause.src = "./img/repeat.svg";
+//     isPlaying = true;
+//     // pauseButton.disabled = false;
+//     breathAnimation();
+//     startBreathing = setInterval(breathAnimation, 8000);
+//     startTimer();
+//     timingInterval = setInterval(startTimer, 1000);
+//   } else {
+//     console.log(circle.classList);
+//     // pauseButton.disabled = true;
+//     imagePause.src = "./img/play.svg";
+//     isPlaying = false;
+//     clearInterval(timingInterval);
+//     clearInterval(startBreathing);
+//     console.log("finsihed");
+//   }
+// };
 
-pauseButton.addEventListener("click", pauseAnimation);
+// pauseButton.addEventListener("click", pauseAnimation);
 
 //timer
 const startTimer = () => {
@@ -90,3 +94,43 @@ const checkSeconds = (sec) => {
   }
   return sec;
 };
+
+//interval
+let secondsCircle = 0;
+const secondsCycle = () => {
+  secondsCircle++;
+  console.log(secondsCircle);
+  if (secondsCircle == 9) {
+    secondsCircle = 1;
+  }
+
+  if (secondsCircle >= 0 && secondsCircle <= 3) {
+    text.innerText = "Breathe In!";
+    circle.className = "breathe__circle grow";
+  } else if (secondsCircle >= 3 && secondsCircle <= 5) {
+    text.innerText = "Hold!";
+    circle.className = "breathe__circle hold";
+  } else if (secondsCircle >= 5 && secondsCircle <= 8) {
+    text.innerText = "Breathe out!";
+    circle.className = "breathe__circle shrink";
+  }
+};
+
+pauseButton.addEventListener("click", () => {
+  if (!isPlaying) {
+    startTimer();
+    timingInterval = setInterval(startTimer, 1000);
+    secondsCycle();
+    startBreathing = setInterval(secondsCycle, 1000);
+    imagePause.src = "./img/pause.svg";
+    isPlaying = true;
+  } else {
+    clearInterval(startBreathing);
+    clearInterval(timingInterval);
+    imagePause.src = "./img/play.svg";
+    isPlaying = false;
+    text.innerHTML = "Paused";
+  }
+});
+// breathAnimation();
+//     startBreathing = setInterval(breathAnimation, 8000);
